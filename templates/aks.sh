48ffd6057fcd5f6cd594bd8882874487b1d5306a
Patrick El-Azem <paelaz@microsoft.com>
2021-04-23 15:00:38 -0400

    Deploy AKS cluster into existing network 13

--- templates/aks.sh
+++ templates/aks.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-az aks create -g always-on-5 -n ao-aks-mi1 \
-	--enable-managed-identity --assign-identity "/subscriptions/e61e4c75-268b-4c94-ad48-237aa3231481/resourcegroups/always-on-5/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pz-ao-uami"
+subscriptionId="$(az account show -o tsv --query 'id')"
+location="eastus"
+resourceGroup="always-on-5"
+templateFile="aks.cluster.json"
 
+clusterName="ao-aks4-eus"
+dnsPrefix="aoaks4eus"
+
+managedIdentityType="UserAssigned"
+identityResourceId="/subscriptions/e61e4c75-268b-4c94-ad48-237aa3231481/resourcegroups/always-on-5/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pz-ao-uami"
+
+nodeAdminUsername="pelazem"
+sshRSAPublicKey="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAg+4FzJlW5nqUa798vqYGanooy5HvSyG8sS6KjPu0sJAf+fkP6qpHY8k1m2/Z9Mahv2Y0moZDiVRHFMGH8qZU+AlYdvjGyjxHcIzDnsmHcV2ONxEiop4KMJLwecHUyf95ogicB1QYfK/6Q8pL9sDlXt8bAcSh6iP0u2d1g9QJaON2aniOpzn68xnKdGT974i7JQLN0SjaPiidZ2prc0cSIMBN26tGV7at2Jh5FIb1Jv8fXHnZebD/vgLilfCqLbuQjTpDVCskZ+OUAyvlBko3gBjRgd/jBprMqCpFLoGUBVkSSR0IkjTj2A6n2XyCyYRMFYrVrjwyU8I+IvO/6zJSEw== pelazem"
+
+vnetName="ao-eus"
+subnetName="App"
+vnetSubnetResourceId="$(az network vnet subnet show --subscription ""$subscriptionId"" -g ""$resourceGroup"" --vnet-name ""$vnetName"" -n ""$subnetName"" -o tsv --query 'id')"
+
+az deployment group create --subscription "$subscriptionId" -n "AKS" --verbose \
+	-g "$resourceGroup" --template-file "$templateFile" \
+	--parameters \
+	location="$location" clusterName="$clusterName" dnsPrefix="$dnsPrefix" \
+	managedIdentityType="$managedIdentityType" identityResourceId="$identityResourceId" \
+	nodeAdminUsername="$nodeAdminUsername" sshRSAPublicKey="$sshRSAPublicKey" \
+	vnetSubnetResourceId="$vnetSubnetResourceId"
+
+az aks show -g "$resourceGroup" -n "$clusterName" --query "servicePrincipalProfile"
+az aks show -g "$resourceGroup" -n "$clusterName" --query "identity"
