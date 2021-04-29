9716ac44bda62212d441f0688e4226a525693183
Patrick El-Azem <paelaz@microsoft.com>
2021-04-29 17:15:29 -0400

    1.APIM 20

--- scripts/aks-net.sh
+++ scripts/aks-net.sh
@@ -10,17 +10,17 @@ publicIpNameAppGw="ao-appgw-pip-eus"
 extIpRange="75.68.47.183/32"
 
 # Get existing authorized IP ranges
-# az aks show --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --query 'apiServerAccessProfile.authorizedIpRanges' --verbose
+ az aks show --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --query 'apiServerAccessProfile.authorizedIpRanges' --verbose
 
 # Disable authorized IP ranges
 # az aks update --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --api-server-authorized-ip-ranges ""
 
-publicIpIds=("$(az network application-gateway frontend-ip list --subscription "$subscriptionId" -g "$resourceGroup" --gateway-name "$appGwName" -o tsv --query "[].publicIpAddress.id")")
+#publicIpIds=("$(az network application-gateway frontend-ip list --subscription "$subscriptionId" -g "$resourceGroup" --gateway-name "$appGwName" -o tsv --query "[].publicIpAddress.id")")
 
-appGwPublicIp="$(az network public-ip show --ids "${publicIpIds[0]}" -o tsv --query "ipAddress")"
+#appGwPublicIp="$(az network public-ip show --ids "${publicIpIds[0]}" -o tsv --query "ipAddress")"
 
-authorizedIps="$extIpRange"",""$appGwPublicIp""/32"
+#authorizedIps="$extIpRange"",""$appGwPublicIp""/32"
 
 # echo $authorizedIps
 
-az aks update --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --api-server-authorized-ip-ranges "$authorizedIps" --verbose
+#az aks update --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --api-server-authorized-ip-ranges "$authorizedIps" --verbose
