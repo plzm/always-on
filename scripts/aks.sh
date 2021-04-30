d524444c57843f32f5ee1191d5471c85675f392e
Patrick El-Azem <paelaz@microsoft.com>
2021-04-30 13:11:14 -0400

    1.APIM 31

--- scripts/aks.sh
+++ scripts/aks.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-subscriptionId="$(az account show -o tsv --query 'id')"
 location="eastus"
-resourceGroup="always-on-9"
-templateFile="../templates/aks.cluster.json"
+
+subscriptionId="$(az account show -o tsv --query 'id')"
+infix="10"
+
+resourceGroup="always-on-""$infix"
 
 clusterName="ao-aks-eus"
 dnsPrefix="aoakseus"
 
+managedIdentityName="pz-ao-uami-""$infix"
 managedIdentityType="UserAssigned"
-identityResourceId="/subscriptions/e61e4c75-268b-4c94-ad48-237aa3231481/resourceGroups/always-on-9/providers/Microsoft.ManagedIdentity/userAssignedIdentities/pz-ao-uami-9"
+identityResourceId="$(az identity show --subscription ""$subscriptionId"" -g ""$resourceGroup"" -n ""$managedIdentityName"" -o tsv --query 'id')"
 
 nodeAdminUsername="pelazem"
 sshRSAPublicKey="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAg+4FzJlW5nqUa798vqYGanooy5HvSyG8sS6KjPu0sJAf+fkP6qpHY8k1m2/Z9Mahv2Y0moZDiVRHFMGH8qZU+AlYdvjGyjxHcIzDnsmHcV2ONxEiop4KMJLwecHUyf95ogicB1QYfK/6Q8pL9sDlXt8bAcSh6iP0u2d1g9QJaON2aniOpzn68xnKdGT974i7JQLN0SjaPiidZ2prc0cSIMBN26tGV7at2Jh5FIb1Jv8fXHnZebD/vgLilfCqLbuQjTpDVCskZ+OUAyvlBko3gBjRgd/jBprMqCpFLoGUBVkSSR0IkjTj2A6n2XyCyYRMFYrVrjwyU8I+IvO/6zJSEw== pelazem"
@@ -24,7 +27,7 @@ appGwName="ao-appgw-eus"
 publicIpNameAppGw="ao-appgw-pip-eus"
 
 #az deployment group create --subscription "$subscriptionId" -n "AKS" --verbose \
-#	-g "$resourceGroup" --template-file "$templateFile" \
+#	-g "$resourceGroup" --template-file "../templates/aks.cluster.json" \
 #	--parameters \
 #	location="$location" clusterName="$clusterName" dnsPrefix="$dnsPrefix" \
 #	managedIdentityType="$managedIdentityType" identityResourceId="$identityResourceId" \
@@ -47,11 +50,11 @@ publicIpNameAppGw="ao-appgw-pip-eus"
 #	publicIpResourceGroup="$resourceGroup" publicIpName="$publicIpNameAppGw"
 
 # Get/overwrite AKS credentials
-az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --overwrite-existing --verbose
+#az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --overwrite-existing --verbose
 
 # Show AKS cluster identity config
 #az aks show -g "$resourceGroup" -n "$clusterName" --query "servicePrincipalProfile"
 #az aks show -g "$resourceGroup" -n "$clusterName" --query "identity"
 
-
+# As this includes an ingress, will take a few minutes for App GW to finish updating
 kubectl apply -f ../aks/azure-vote.yaml
