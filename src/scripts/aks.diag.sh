0fd1bc05d3d94f8217062e2a7325caceb6864ce0
Patrick El-Azem <paelaz@microsoft.com>
2021-05-17 10:45:06 -0400

    Add AKS pod identity

--- src/scripts/aks.diag.sh
+++ src/scripts/aks.diag.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+PREFIX="pz-ao"
+SUFFIX="22"
+
 subscriptionId="$(az account show -o tsv --query 'id')"
 location="eastus"
+
+AZURE_RESOURCE_GROUP=always-on-$location
+UAMI_NAME=$PREFIX-$location
+
 resourceGroup="always-on-""$location"
 clusterName="pz-ao-""$location"
+
+UAMI_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP -n $UAMI_NAME -o tsv --query 'id')
+UAMI_PRINCIPAL_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP -n $UAMI_NAME -o tsv --query 'principalId')
+UAMI_CLIENT_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP -n $UAMI_NAME -o tsv --query 'clientId')
 
 az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --overwrite-existing --verbose
 
