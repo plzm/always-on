2c007f46807eb061435080ac3b66f57401ccebf1
Patrick El-Azem <paelaz@microsoft.com>
2021-05-20 09:44:35 -0400

    Fix infra config region and app deploy to correctly add new AKV secrets all the way through to env vars

--- src/scripts/aks.diag.sh
+++ src/scripts/aks.diag.sh
@@ -43,5 +43,5 @@ az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "
 #kubectl -n default rollout restart deploy
 
 # Shell to a pod
-#kubectl exec --stdin --tty azure-vote-front-64484d8754-rpm6c -- /bin/bash
+#kubectl exec --stdin --tty azure-vote-back-665b65975f-g2kdg -- /bin/bash
 
