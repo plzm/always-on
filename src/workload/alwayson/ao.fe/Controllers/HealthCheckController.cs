00297b6b6635603b41f40109935f573fc2321451
Patrick El-Azem <paelaz@microsoft.com>
2021-05-31 05:54:31 -0400

    Add healthcheck to FE for AppGW back end provisioning

---
+++ src/workload/alwayson/ao.fe/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ao.fe.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthCheckController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult Get()
+		{
+			return new OkResult();
+		}
+	}
+}
