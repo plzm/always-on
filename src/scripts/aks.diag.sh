194e4803c306df93c4e068e0c67f158cbb1043c0
Patrick El-Azem <paelaz@microsoft.com>
2021-06-10 14:45:29 -0400

    Fix AKV secret name

--- src/scripts/aks.diag.sh
+++ src/scripts/aks.diag.sh
@@ -45,5 +45,5 @@ az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "
 s
 
 # Shell to a pod
-#kubectl exec --stdin --tty ao-be-649dbb95c-4jvp6 -- /bin/bash
+#kubectl exec --stdin --tty ao-be-649dbb95c-hckhm -- /bin/bash
 
