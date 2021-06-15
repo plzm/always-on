41a51378576a84ffba9b0d91c57b7a48d60d82bc
Patrick El-Azem <paelaz@microsoft.com>
2021-06-15 17:52:51 -0400

    Prep new deployment

--- src/scripts/aks.diag.sh
+++ src/scripts/aks.diag.sh
@@ -45,5 +45,5 @@ az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "
 s
 
 # Shell to a pod
-#kubectl exec --stdin --tty azure-vote-back-8598b585b-rwxbf -- /bin/bash
+#kubectl exec --stdin --tty ao-be-d7cbf9675-lt8lp -- /bin/bash
 
