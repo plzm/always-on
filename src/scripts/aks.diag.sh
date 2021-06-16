6c1d530d2eba7b91c1f91e52f333a8796380dc3e
Patrick El-Azem <paelaz@microsoft.com>
2021-06-16 05:55:34 -0400

    Add env vars to FE pod manifest

--- src/scripts/aks.diag.sh
+++ src/scripts/aks.diag.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
 PREFIX="pz-ao"
-SUFFIX="31"
+SUFFIX="34"
+location="eastus2"
 
 subscriptionId="$(az account show -o tsv --query 'id')"
-location="westus2"
 resourceGroup="always-on-""$location"
 clusterName="pz-ao-""$location"
 
-AZURE_RESOURCE_GROUP_GLOBAL=always-on-global
-AZURE_RESOURCE_GROUP=always-on-$location
-UAMI_NAME=$PREFIX
-
-
-#UAMI_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP_GLOBAL -n $UAMI_NAME -o tsv --query 'id')
-#UAMI_PRINCIPAL_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP_GLOBAL -n $UAMI_NAME -o tsv --query 'principalId')
-#UAMI_CLIENT_ID=$(az identity show --subscription $subscriptionId -g $AZURE_RESOURCE_GROUP_GLOBAL -n $UAMI_NAME -o tsv --query 'clientId')
-
-#echo "Resource ID: ""$UAMI_ID"
-#echo "Principal ID: ""$UAMI_PRINCIPAL_ID"
-#echo "Client ID: ""$UAMI_CLIENT_ID"
-
 # Purge kubectl
-#kubectl config unset current-context
-#kubectl config unset contexts
-#kubectl config unset clusters
-#kubectl config unset users
+kubectl config unset current-context
+kubectl config unset contexts
+kubectl config unset clusters
+kubectl config unset users
 #kubectl config view
 
 az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "$clusterName" --overwrite-existing --verbose
+
+# Shell to a pod
+pod=ao-fe-86595b789f-twt9z
+kubectl exec --stdin --tty $pod -- /bin/bash
 
 #kubectl get deployments -A -o wide
 #kubectl get nodes -A -o wide
@@ -42,8 +33,3 @@ az aks get-credentials --subscription "$subscriptionId" -g "$resourceGroup" -n "
 # Bounce
 #kubectl -n default rollout restart deployment
 #kubectl -n default rollout restart daemonset
-
-
-# Shell to a pod
-#kubectl exec --stdin --tty ao-be-56bfff7985-89pvt -- /bin/bash
-
