f9ef5399383f5520769164119adf8cfb2c60bae0
Patrick El-Azem <paelaz@microsoft.com>
2021-06-21 07:14:06 -0400

    Update delete script

--- src/scripts/delete-all.sh
+++ src/scripts/delete-all.sh
@@ -2,4 +2,8 @@
 
 az group delete -g always-on-eastus2 --yes
 az group delete -g always-on-westus2 --yes
+
+az group delete -g always-on-tests --yes
+#az group delete -g always-on-ops --yes
+
 az group delete -g always-on-global --yes
