a2bfd9cd74ece337e1e49779b853229c095221ed
Patrick El-Azem <paelaz@microsoft.com>
2021-07-13 13:27:40 -0400

    Move in-region App Insights later after Log Analytics

--- src/scripts/delete-all.sh
+++ src/scripts/delete-all.sh
@@ -3,7 +3,7 @@
 az group delete -g always-on-eastus2 --yes
 az group delete -g always-on-westus2 --yes
 
-az group delete -g always-on-tests --yes
+#az group delete -g always-on-tests --yes
 #az group delete -g always-on-ops --yes
 
-az group delete -g always-on-global --yes
+#az group delete -g always-on-global --yes
